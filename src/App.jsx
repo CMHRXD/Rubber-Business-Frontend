fe4a09838e722e490fc91658b2d4d1adc0514641
Cristian Hernandez <cmhrlxl@gmail.com>
2023-05-29 13:31:20 +0100

    Modify font

--- src/App.jsx
+++ src/App.jsx
@@ -11,26 +11,7 @@ import { ServiceContextProvider } from './context/ServiceContextProvider';
 import { ClientsContextProvider } from './context/ClientsContextProvider';
 
 import PageAnimation from './components/PageAnimation';
-/*
-//Layout
-import AuthLayout from './views/layouts/AuthLayout';
 
-//Public Views
-import Login from './views/Login';
-import SignUp from './views/SignUp';
-import ConfirmAccount from './views/ConfirmAccount';
-import PswForgot from './views/PasswordForgot';
-import NewPassword from './views/NewPassword';
-
-//Private View
-import ProtectedLayout from './views/layouts/ProtectedLayout';
-import CartView from './views/CartView';
-import ProfileView from './views/ProfileView';
-import ProductView from './views/ProductView';
-import SellsView from './views/SellsView';
-import TempCartView from './views/TempCartView';
-import DetailView from './views/DetailView';
-* */
 function App() {
 
   return (
